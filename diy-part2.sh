77e5bfb570ccb7fea304e465eafe1544ba2ee683
Ucopyrightable <132915653+Ucopyrightable@users.noreply.github.com>
2024-03-08 15:25:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,5 +25,6 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,chinadns-ng,mosdns,smartdns}
 rm -rf feeds/smpackage/luci-app-adguardhome
 rm -rf feeds/smpackage/luci-app-bypass
+rm -rf feeds/luci/luci-app-smartdns
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
