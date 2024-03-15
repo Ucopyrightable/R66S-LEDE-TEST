32315573fab229095cb48d53dbf4a08690e791ff
Ucopyrightable <132915653+Ucopyrightable@users.noreply.github.com>
2024-03-15 20:17:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,15 @@ sed -i '1i src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 sed -i '2i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
 sed -i '3i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '4i src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+
+#Add Openclash
+mkdir package/modify
+mkdir package/modify/openclash
+cd package/modify/openclash
+git init
+git remote add origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+cd ../../..
